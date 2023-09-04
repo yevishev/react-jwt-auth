a89252849ac94714b996208836b43f3558e7aa92
yevishev <yevishev.m@gmail.com>
2023-09-04 03:46:07 +0300

    added logic of email validation

--- frontend/src/components/authPage.jsx
+++ frontend/src/components/authPage.jsx
@@ -5,52 +5,78 @@ import logoGit from '../static/img/git.svg';
 import logoGoogle from '../static/img/ggl.svg';
 
 export default (() => {
-    const [showPasswordInput, setShowPasswordInput] = useState(false);
-    const [email, setEmail] = useState('');
-
-    const handelEmailInput = (e) => {
-        setEmail(e.target.value);
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
     }
 
-    useEffect(() => {
-        console.log(email);
-    }, [email]);
+    const [showPasswordInput, setShowPasswordInput] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(null);
+    const [borderColor, setBorderColor] = useState('1px solid rgb(226, 226, 226)');
 
-    const handleContinueClick = () => {
+    const handelEmailChange = e => {
+        setEmailError(null);
+        setEmail(e.target.value);
+        setBorderColor('1px solid rgb(226, 226, 226)');
+        setShowPasswordInput(false);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+        console.log(password);
+    };
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        if (!isValidEmail(email)) {
+            setEmailError('Email is invalid');
+            setBorderColor('1px solid rgb(254, 71, 71)');
+            return;
+        }
+
+        setEmailError(null);
         setShowPasswordInput(true);
     };
 
     return (
         <div className="container">
             <div className="block">
-                <div className="logo-block" >
-                    <img src={mainLogo} className="logo-block__img" alt="" />
-                </div>
-                <div className="welcome-text-block" >
-                    <p className="wtb__header">Welcome</p>
-                    <p className="wtb__text">Log in to RanCo to continue to RanCo.</p>
-                </div>
-                <div className="input-block">
-                    <input
-                        onInput={handelEmailInput}
-                        type="email"
-                        className="eib__input"
-                        placeholder="Email"
-                    />
-                </div>
-                {showPasswordInput && (
-                    <div className="input-block">
-                        <input type="password" className="eib__input" placeholder="Password" />
+                <form onSubmit={handleSubmitForm}>
+                    <div className="logo-block" >
+                        <img src={mainLogo} className="logo-block__img" alt="" />
                     </div>
-                )}
-                <div className="button-continue-block">
-                    <button
-                        onClick={handleContinueClick}
-                        type="button"
-                        className="btb__button">
-                        Continue
-                    </button>
-                </div>
+                    <div className="welcome-text-block" >
+                        <p className="wtb__header">Welcome</p>
+                        <p className="wtb__text">Log in to RanCo to continue to RanCo.</p>
+                    </div>
+                    <div className="input-block">
+                        <input
+                            onChange={handelEmailChange}
+                            type="text"
+                            className="eib__input"
+                            placeholder="Email"
+                            style={{ border: borderColor }}
+                        />
+                    </div>
+                    {emailError && <p style={{ color: 'red' }}>Email is invalid</p>}
+                    {showPasswordInput && (
+                        <div className="input-block">
+                            <input
+                                onChange={handlePasswordChange}
+                                type="password"
+                                className="eib__input"
+                                placeholder="Password" />
+                        </div>
+                    )}
+                    <div className="button-continue-block">
+                        <button
+                            type="submit"
+                            className="btb__button">
+                            Continue
+                        </button>
+                    </div>
+                </form>
                 <div className="text-or-block">
                     <p>or</p>
                 </div>
