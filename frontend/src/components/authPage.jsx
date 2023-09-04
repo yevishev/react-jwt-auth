60cff35ed11e52ea3b6ea1c86dd78e86c4e2612f
yevishev <yevishev.m@gmail.com>
2023-09-04 04:02:56 +0300

    excluding form authPage as single component

--- frontend/src/components/authPage.jsx
+++ frontend/src/components/authPage.jsx
@@ -1,82 +1,20 @@
-import React, { useEffect, useState } from 'react';
-
+import Form from './Form';
 import mainLogo from '../static/img/random-logo-png-transparent.png';
 import logoGit from '../static/img/git.svg';
 import logoGoogle from '../static/img/ggl.svg';
 
 export default (() => {
-    function isValidEmail(email) {
-        return /\S+@\S+\.\S+/.test(email);
-    }
-
-    const [showPasswordInput, setShowPasswordInput] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState(null);
-    const [borderColor, setBorderColor] = useState('1px solid rgb(226, 226, 226)');
-
-    const handelEmailChange = e => {
-        setEmailError(null);
-        setEmail(e.target.value);
-        setBorderColor('1px solid rgb(226, 226, 226)');
-        setShowPasswordInput(false);
-    }
-
-    const handlePasswordChange = e => {
-        setPassword(e.target.value);
-        console.log(password);
-    };
-
-    const handleSubmitForm = (e) => {
-        e.preventDefault();
-        if (!isValidEmail(email)) {
-            setEmailError('Email is invalid');
-            setBorderColor('1px solid rgb(254, 71, 71)');
-            return;
-        }
-
-        setEmailError(null);
-        setShowPasswordInput(true);
-    };
-
     return (
         <div className="container">
             <div className="block">
-                <form onSubmit={handleSubmitForm}>
-                    <div className="logo-block" >
-                        <img src={mainLogo} className="logo-block__img" alt="" />
-                    </div>
-                    <div className="welcome-text-block" >
-                        <p className="wtb__header">Welcome</p>
-                        <p className="wtb__text">Log in to RanCo to continue to RanCo.</p>
-                    </div>
-                    <div className="input-block">
-                        <input
-                            onChange={handelEmailChange}
-                            type="text"
-                            className="eib__input"
-                            placeholder="Email"
-                            style={{ border: borderColor }}
-                        />
-                    </div>
-                    {emailError && <p style={{ color: 'red' }}>Email is invalid</p>}
-                    {showPasswordInput && (
-                        <div className="input-block">
-                            <input
-                                onChange={handlePasswordChange}
-                                type="password"
-                                className="eib__input"
-                                placeholder="Password" />
-                        </div>
-                    )}
-                    <div className="button-continue-block">
-                        <button
-                            type="submit"
-                            className="btb__button">
-                            Continue
-                        </button>
-                    </div>
-                </form>
+                <div className="logo-block" >
+                    <img src={mainLogo} className="logo-block__img" alt="" />
+                </div>
+                <div className="welcome-text-block" >
+                    <p className="wtb__header">Welcome</p>
+                    <p className="wtb__text">Log in to RanCo to continue to RanCo.</p>
+                </div>
+                <Form />
                 <div className="text-or-block">
                     <p>or</p>
                 </div>
