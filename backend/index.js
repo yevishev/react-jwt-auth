b71dc20e8cf7aabdc61e711f64727aaf0133812a
yevishev <yevishev.m@gmail.com>
2023-09-09 00:58:57 +0300

    added handling error and finalized business logic of authorization

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import sqlite3 from 'sqlite3';
 
 const app = express();
 const db = new sqlite3.Database('users.db');
+
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -26,97 +27,75 @@ app.use((req, res, next) => {
         next();
     }
 });
+
 app.use(express.json());
 app.use(cookieParser());
+
+const dbRun = (query, args) => {
+    return new Promise((resolve, reject) => {
+        db.get(query, args, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
 
 app.post('/session', async (req, res) => {
     const email = req.body.email;
     const salt = getSalt();
+    const passwordHash = await getPasswordHash(req.body.password, salt, 10000);
 
-    try {
-        const passwordHash = await getPasswordHash(req.body.password, salt, 10000);
+    const row = await dbRun('SELECT * FROM users WHERE email = ? and password_hash = ?', [email, passwordHash]);
 
-        db.get('SELECT * FROM users WHERE email = ?', [email], (err, row) => {
-            if (err) {
-                return res.sendStatus(500);
-            } 
-            
-            if (row) {
-                db.get('SELECT * FROM users WHERE email = ? and password_hash = ?', [email, passwordHash], (err, row) => {
-                    if (err) {
-                        return res.sendStatus(500);
-                    } 
+    if (!row) {
+        try {
+            await dbRun('INSERT INTO users (email, password_hash) VALUES (?, ?)', [email, passwordHash]);
+        } catch(err) {
+            console.error('Error in /session route:', err.toString());
+            res.status(403).json({ error: 'Invalid email or password' });
+            return;
+        }
+    
+    };
 
-                    if (row) {
-                        db.get('SELECT * FROM users WHERE email = ? and password_hash = ?', [email, passwordHash], (err, row) => {
-                            if (err) {
-                                return res.sendStatus(500);
-                            } 
+    const token = createJWT({
+        email
+    });
 
-                            if (row) {
-                                const token = createJWT({
-                                    email: row.email
-                                });
-
-                                const expirationDate = new Date();
-                                expirationDate.setDate(expirationDate.getDate() + 7);
-                                res.status(200);
-                                res.cookie('token', token, { expires: expirationDate, httpOnly: true });
-                                return res.send();
-                            } else {
-                                return res.sendStatus(500);
-                            }
-                        });
-                    }
-
-                    return res.sendStatus(400);
-                });
-            } else {
-                db.run('INSERT INTO users (email, password_hash) VALUES (?, ?)', [email, passwordHash], (err) => {
-                    if (err) {
-                        return res.sendStatus(500);
-                    }
-
-                    const token = createJWT({
-                        email: email
-                    });
-
-                    const expirationDate = new Date();
-                    expirationDate.setDate(expirationDate.getDate() + 7);
-                    res.status(200);
-                    res.cookie('token', token, { expires: expirationDate, httpOnly: true });
-
-                    return res.send();
-                });
-            }
-        });
-    } catch (error) {
-        console.error("error is \n\n\n\n" , error);
-        return res.sendStatus(500);
-    }
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 7);
+    res.cookie('token', token, { expires: expirationDate, httpOnly: true });
+    res.status(200).json();
 });
 
-app.get('/session', async (req, res) => {
-    const cookie = req.cookies;
-    try {
-        const data = await readJWT(cookie.token);
-        data.res = true;
-        res.write(JSON.stringify(data));
-    } catch (e) {
-        res.write(JSON.stringify({ res: false }));
-    }
-    res.send();
+app.get('/session', (req, res) => {
+    const cookies = req.cookies
+    readJWT(cookies.token)
+        .then(() => {
+            res.status(200).json();
+        })
+        .catch(error => {
+            console.error('Error in /session route:', error);
+            res.status(403).json({ error: 'User is unauthorized' });
+        });
 });
 
 app.delete('/session', async (req, res) => {
-    try {
-        res.clearCookie('token');
-    } catch (e) {
-        console.log("err is ", e);
-        res.status(400);
-    }
-    res.send();
+    res.clearCookie('token');
+    return res.status(200).json();
 });
+
+
+// app.use((err, req, res, next) => {
+//     if (!!err.statusCode) {
+//         res.status(err.statusCode).json(err.body || null);
+//     } else {
+//         res.status(500).send(err.toString());
+//     }
+// });
 
 app.listen(8081, () => {
     console.log('App listening port :8081');
